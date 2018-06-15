884a1cf663d7e855e3eb8001c437d7e8fe0a2d3b
ravindrae <ravindraleader@gmail.com>
2018-06-15 10:27:05 +0530

    a

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!,except:[:show ,:index]
-
 #before_action :authenticate_user
   def index
-
+    @articles=Article.all
   end
 
   def show

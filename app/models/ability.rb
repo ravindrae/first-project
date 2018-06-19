63803b2f7c95055770dea31f297e1b1fb9f7a094
ravindrae <ravindraleader@gmail.com>
2018-06-19 11:16:50 +0530

    updated

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,27 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-  #     if user.admin?
-    #     can :manage, :all
-    #   else
-  #       can :read, :all
-  #     end
+    user ||= User.new # guest user (not logged in)
+
+    if user.admin?
+     can :manage, :all
+    else
+      can :update,Article do |article|
+        article.user==user
+      end
+      can :destroy,Article do |article|
+        article.user==user
+      end
+      can :destroy,Comment do |comment|
+        comment.user ==user
+      end
+      can :update,Comment do |comment|
+        comment.user ==user
+      end
+      can :create,Article
+      can :create,Comment
+      can :read, :all
+   end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_13_060125) do
+ActiveRecord::Schema.define(version: 2018_06_19_043531) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_06_13_060125) do
     t.string "username"
     t.text "address"
     t.date "dateofbirth"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

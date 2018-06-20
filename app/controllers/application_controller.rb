4378379296ac2306bbff4411712f7038f1426400
ravindrae <ravindraleader@gmail.com>
2018-06-20 10:15:27 +0530

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up ,keys:[:first_name,:last_name,:email,:password,:dateofbirth,:remember_me])
-   devise_parameter_sanitizer.permit(:sign_in,keys:[:email,:password,:remember_me])
-   devise_parameter_sanitizer.permit(:account_update,keys:[:first_name,:last_name,:email,:password,:dateofbirth,:address,:remember_me])
+    devise_parameter_sanitizer.permit(:sign_up ,keys:[:first_name,:last_name,:dateofbirth])
+   devise_parameter_sanitizer.permit(:account_update,keys:[:first_name,:last_name,:dateofbirth,:address])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_043531) do
+ActiveRecord::Schema.define(version: 2018_06_20_043516) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_06_19_043531) do
     t.text "address"
     t.date "dateofbirth"
     t.boolean "admin"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
